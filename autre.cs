714b078f0ec5ba6b228fc04eb670a342e5ece452
Emmanuel Francis Constant <78762241+efconstant@users.noreply.github.com>
2023-08-25 11:22:36 -0400

    Add files via upload
    Fichiers à clone

---
+++ autre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple
+{
+    internal class Item
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Item2(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
